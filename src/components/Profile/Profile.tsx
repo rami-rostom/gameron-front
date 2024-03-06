a5e7c3a5b841562827f22be6f79cab6af6e33c86
Rami <rami.rostom@proton.me>
2024-03-06 19:16:17 +0100

    :sparkles: feat: add hello username in profile when user is logged

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '@/hooks/redux';
 
 import { CircleUserRound } from 'lucide-react';
@@ -7,6 +7,8 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 
@@ -17,6 +19,7 @@ function Profile() {
   const navigate = useNavigate();
 
   const isLogged = useAppSelector((state) => state.login.logged);
+  const username = useAppSelector((state) => state.login.data.username);
 
   // Function to empty local storage and to disconnect user
   const handleLogout = () => {
@@ -35,6 +38,10 @@ function Profile() {
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
+            <DropdownMenuLabel className="capitalize">
+              Hello {username}
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
             <DropdownMenuItem onClick={handleLogout}>
               Déconnexion
             </DropdownMenuItem>

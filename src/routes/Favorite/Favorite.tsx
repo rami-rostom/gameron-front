7b2f9bfe56f94767875891e6313ad2ea8195b49f
Rami <rami.rostom@proton.me>
2024-03-16 12:49:16 +0100

    :sparkles: feat: dynamic render of favorite game page

--- src/routes/Favorite/Favorite.tsx
+++ src/routes/Favorite/Favorite.tsx
@@ -1,8 +1,34 @@
+import GameCard from '@/components/GameCard/GameCard';
+import { useAppDispatch, useAppSelector } from '@/hooks/redux';
+import { getFavoriteGames } from '@/store/reducers/getFavoriteGames';
+import { useEffect } from 'react';
+
 function Favorite() {
+  const dispatch = useAppDispatch();
+
+  const userId = useAppSelector((state) => state.login.data.userId);
+  const favoriteGames = useAppSelector((state) => state.favoriteGames.game);
+
+  useEffect(() => {
+    dispatch(getFavoriteGames(userId));
+  }, [dispatch, userId]);
+
   return (
     <>
-      <div>Favorite</div>
-      <div>Favorite</div>
+      <div className="home">
+        {Object.values(favoriteGames).map((game) => (
+          <GameCard
+            key={game.gameId}
+            id={game.gameId}
+            name={game.name}
+            slug={game.slug}
+            genres={[]}
+            released={game.released}
+            rating={game.rating}
+            thumbnail={game.background_image}
+          />
+        ))}
+      </div>
     </>
   );
 }
